83ea0a8dd83abd3025e5b025b3abc34d3b1351b4
Naxrul Ahmed <najrul787@gmail.com>
2022-04-24 14:52:54 +0600

    Fix isScrollDown

--- example/index.tsx
+++ example/index.tsx
@@ -6,7 +6,12 @@ import Scrollbar from '../.';
 const App = () => {
   return (
     <div style={{width: 200, height: 500}}>
-      <Scrollbar autoHide>
+      <Scrollbar 
+        autoHide
+        onScroll={(e: any, optins) => {
+          console.log(optins);
+        }}
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nisi, ea recusandae ipsam officiis ut perferendis dolorem commodi culpa blanditiis voluptate facilis et sed autem alias consequatur laboriosam reprehenderit harum.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nisi, ea recusandae ipsam officiis ut perferendis dolorem commodi culpa blanditiis voluptate facilis et sed autem alias consequatur laboriosam reprehenderit harum.
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur nisi, ea recusandae ipsam officiis ut perferendis dolorem commodi culpa blanditiis voluptate facilis et sed autem alias consequatur laboriosam reprehenderit harum.
